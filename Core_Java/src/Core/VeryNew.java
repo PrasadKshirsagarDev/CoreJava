52c0221b38176af003a4bdf5e22ee77f6518885c
PrasadKshirsagarDev <prasadshirsagar291@gmail.com>
2025-08-26 18:23:26 +0530

    VeryNew

---
+++ Core_Java/src/Core/VeryNew.java
@@ -0,0 +1,28 @@
+package Core;
+
+public class VeryNew {
+
+	public static void main(String[] args) {
+		int num = 13;
+		
+		boolean flag = false;
+		
+	    
+		for(int i=2; i<13; i++)
+		{
+			if(num%i==0) {
+				flag = true;
+			}
+		}
+			if(flag == true)
+			{
+				System.out.println("Composite" +num);
+			}
+			else
+			{
+				System.out.println("Prime" +num);
+			}
+		
+	}
+
+}
