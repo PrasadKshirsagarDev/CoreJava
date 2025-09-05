17891146eaec1e5375fb89bf1e722a8d1dde548c
PrasadKshirsagarDev <prasadshirsagar291@gmail.com>
2025-09-05 17:18:53 +0530

    Diagonal_Element

---
+++ Core_Java/src/Jagged/Second.java
@@ -0,0 +1,49 @@
+package Jagged;
+
+public class Second {
+
+	public static void main(String[] args) {
+		int [][] jagged = new int [6][];
+		
+		jagged[0] = new int[1];
+		jagged[1] = new int[2];
+		jagged[2] = new int[3];
+		jagged[3] = new int[4];
+		jagged[4] = new int[5];
+		jagged[5] = new int[6];
+		
+		
+		int count=1;
+		for(int i=0; i<6; i++)
+		{
+			for(int j=0; j<jagged[i].length; j++)
+			{
+				jagged[i][j]=count;
+				count++;
+			}
+		}
+		
+		
+		for(int i=0; i<6; i++)
+		{
+			for(int j=0; j<jagged[i].length; j++)
+			{
+				System.out.print(jagged[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("*****************");
+		
+		for(int i=5; i>=0; i--)
+		{
+			 for(int j=jagged[i].length-1; j>=0; j--)
+			 {
+				 System.out.print(jagged[i][j]+" ");
+			 }
+			 System.out.println();
+		}
+
+	}
+
+}
